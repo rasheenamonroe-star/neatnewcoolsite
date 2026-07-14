b79d3744f31605bef6c5fd504db44d6594a3338e
rasheenamonroe-star <rasheenamonroe@gmail.com>
2026-07-14 04:28:15 -0400

    done!

--- components/about-copy-page-component.js
+++ components/about-copy-page-component.js
@@ -3,9 +3,12 @@ export default {
   template: /* html */ `
     <section class="container py-4 about-creator-page-center">
       <h1 class="w-50">About The Creator: Ra'Sheena Monroe</h1>
-      <p class="w-50">
-        Ra'Sheena Monroe (or commonly reffered to as Renee in her group) Is a young adult who has a passion for art and a curiosity that seems to grow more and more as time goes by. She graduated high school in 2026 and in her time attending, she specialized in art. Her main interests include nature, animals, history and pop culture (mostly video games and animated series.)
-      </p>
+      <div class="about-creator-intro">
+        <img src="./assets/Art for webapp/thisisme.png" alt="This is me" class="about-creator-image" />
+        <p class="w-50">
+          Ra'Sheena Monroe (or commonly reffered to as Renee in her group) Is a young adult who has a passion for art and a curiosity that seems to grow more and more as time goes by. She graduated high school in 2026 and in her time attending, she specialized in art. Her main interests include nature, animals, history and pop culture (mostly video games and animated series.)
+        </p>
+      </div>
       <h2 class="h1 mt-4 w-50">Her Teenworks and Nextech Story</h2>
       <p class="w-50">
        Ra'Sheena worked for Teenworks before in 2025 at the Cardinal Greenway. She was tasked to exterminate invasive species of plants. When she signed up for teenworks in 2026, at first she was going to work at IU health but things changed. She was offered to work at Nextech and she accepted. When she first started she didnt know anything about coding but after a few weeks she finally got the hang of it and found it fun. 
